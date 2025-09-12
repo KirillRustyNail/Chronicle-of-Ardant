5d241c5e7c77502bf8b2e51e0976385e33f30855
Anshakov Kirill Igorevich <kkk.rbhbkk@bk.ru>
2025-09-13 00:18:26 +0300

    404

--- Chronicle of Ardant/src/index.jsx
+++ Chronicle of Ardant/src/index.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import App from './App.jsx';
 import './styles.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+const params = new URLSearchParams(window.location.search);
+const redirect = params.get("redirect");
+if (redirect) {
+  window.history.replaceState(null, "", "/Chronicle-of-Ardant" + redirect);
+}
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
